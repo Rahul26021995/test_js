f40a4323be100054fad5c7bc0b84f4f58cf03ca8
Rahul26021995 <rahulrblr1995@gmail.com>
2025-08-06 16:41:21 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+
+import { add, multiply } from './mathUtils.js';
+
+const x = 5;
+const y = 3;
+
+console.log(`Sum: ${add(x, y)}`);        // Output: Sum: 8
+console.log(`Product: ${multiply(x, y)}`); // Output: Product: 15
